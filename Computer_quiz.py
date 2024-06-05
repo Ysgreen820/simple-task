c6758022c1d8d7421ade4d077758a4db78b8fc33
Ysgreen820 <ygreen@fwparker.org>
2024-06-05 10:28:25 -0500

    Add files via upload

---
+++ Computer_quiz.py
@@ -0,0 +1,26 @@
+print("welcome to my computer quiz")
+playing=input("do you want to play? ")
+print(playing)
+if playing!="yes":
+    quit()
+print("okay! Let's play")
+answer=input("Are you 5'9? ")
+if answer=="yes":
+    print("Great!")
+else:
+    print("incorrect")
+    answer=input("Do you have big glasses? ")
+if answer=="yes":
+    print("nice!")
+else:
+    print("incorrect")
+answer=input("Do you have pretty curly hair? ")
+if answer=="yes":
+    print("fantastic!")
+else:
+    print("incorrect")
+answer=input("DO you love me ?")
+if answer=="yes":
+    print("Yay, I love you too!")
+else:
+    print("incorrect")
